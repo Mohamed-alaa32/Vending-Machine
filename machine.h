c009631d1e00cd7eb152088e75096a72777b2bb8
Mohamed Alaa <mohammed.alaa200080@Gmail.com>
2023-12-30 17:42:00 +0200

    finalizing header file

--- machine.h
+++ machine.h
@@ -1,22 +1,34 @@
-
+/***********************************************************************************************************************/
+/**************   Team Members: Mohamed Alaa, Adham Waleed, Marwan Mahmoud  ********************************************/
+/**************                 DATE: 30/12/2023                            ********************************************/
+/**************                   VERSION: 0.1                              ********************************************/
+/**************                 FILE: machine.h                             ********************************************/
+/***********************************************************************************************************************/
 
 #include <iostream>
 #include <vector>
-#include <conio.h>
 #include <ctime>
 using namespace std;
 
 
-
+/*
+ This class defines the product type and it's attributes
+Attributes:
+    1-Name
+    2-Price
+    3-Count
+    4-Expiration Date
+*/
 class Product{
 private:
+    //Private attributes of class product
     string name;
     float price;
     int count;
     long expirationDate;
     string outlet;
-public:
 
+public:
     //Constructor 1
     Product(){
         name = "EMPTY";
@@ -25,61 +37,71 @@ public:
         expirationDate = 0;
         outlet = "None";
     }
-
     //Constructor 2
-    Product(string inputName,float inputPrice,int inputCount, string inputOutlet);
+    Product(string inputName,float inputPrice,int inputCount, string inputOutlet, long expirationDateArray[3]);
+
     //GETTERS
-    string const getName();
-    float const getPrice();
-    int const getCount();
-    string const getOutlet();
+    string const getName(); //returns name of product
+    float const getPrice(); //returns price of product
+    int const getCount();   //returns how many items left for product
+    string const getOutlet();   //returns name of outlet the product dispense from
 
     //SETTERS
-    void setName(string n);
-    void setPrice(float n);
-    void setCount(int n);
-    void setOutlet(string n);
-    void setExpiry(int day,int month,int year);
+    void setName(string n); //Sets name of the product
+    void setPrice(float n); //Sets price of the product
+    void setCount(int n);  //Sets how many items are available for the product
+    void setOutlet(string n); //Sets which outlet the product will dispense from
+    void setExpiry(int day,int month,int year); //Sets expiration date of product in form dd/mm/yyyy
 
-    //Restock
+    //Restock product or adds to the already existing products
     void restockProduct(int itemsAdded);
 
+    //Create a new product and set it's Name, Outlet, count, Expiration date
     void addItem();
 
+    //Checks if the product is expired or not
     bool isExpired();
 
-    //Updating count after making a successful purchase
+    //Updates product count after making a successful purchase
     void oneItemTaken();
 };
 
 
-/*
-* This class contains vector of class "product" named "items"
-* programming mode processes
-* operation mode processes
-*/
 
+
+/*
+* This class defines the vending machine object, stores all products in vector of class "Product",
+The class also contains programming mode, operation mode definitions
+
+Attributes:
+    1-items (Vector contains all products)
+    2-key (Password to enter programming mode)
+    3-finishFlag (Flag to exit from the machine menu)
+*/
 class VendingStorage{
 private:
+    //Private attributes for VendingStorage class
     vector<Product> items;
-    int key = 123;  // used as password to enter programming mode
+    int key = 123;
     bool finishFlag = false;
-    //Today's Date: date
+
 public:
-    //Programming Mode
-    void programmingMode();
-    bool getFinishFlag();
-    void displayItemsList();
 
     //Used for manual adding of items before initializing the code
     void addProduct(Product item);
-    //Operating Mode
+
+    //Operating Mode Functions
     void operationMode();
     void displayMenu();
-    int numberOfProducts();
+    bool getFinishFlag();
     int SelectItemNumber();
     void moneyGrabber(int outletNumber);
     void returnChange(float money, int outletNumber);
     void dispenseItem(int outletNumber);
+
+    //Programming Mode Functions
+    void programmingMode();
+    void displayItemsList();
+    int numberOfProducts();
 
 };
